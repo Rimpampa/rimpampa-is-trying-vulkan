960b42b74ceb64bab125ee9258a8d862e5b8b33c
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2022-02-23 19:36:33 +0100

    Add ouroboros to gruop the vk related state in a single struct

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,81 @@ use winit::window as win;
 
 mod vku;
 
+#[ouroboros::self_referencing]
+struct VulkanState<'a> {
+    instance: vku::Instance<'a>,
+
+    #[cfg(debug_assertions)]
+    #[covariant]
+    #[borrows(instance)]
+    debug: vku::DebugUtils<'this>,
+
+    #[covariant]
+    #[borrows(instance)]
+    surface: vku::Surface<'this, 'a>,
+
+    #[covariant]
+    #[borrows(instance)]
+    phy_devs: Vec<vku::PhysicalDev<'this>>,
+
+    #[covariant]
+    #[borrows(phy_devs, surface)]
+    logic_dev: vku::LogicalDev<'this>,
+}
+
+impl<'a> VulkanState<'a> {
+    fn create(entry: &'a ash::Entry, window: &'a win::Window) -> Self {
+        let validation_layers = vec![
+            cstr!(VK_LAYER_KHRONOS_validation).as_ptr(),
+            // ...
+        ];
+
+        let mut extensions = vec![
+            cstr!(VK_EXT_debug_utils).as_ptr(),
+            cstr!(VK_KHR_surface).as_ptr(),
+            // ...
+        ];
+
+        extensions.extend(
+            vku::Surface::extensions(&window)
+                .unwrap()
+                .into_iter()
+                .map(ffi::CStr::as_ptr),
+        );
+
+        let instance = unsafe {
+            vku::Instance::new(
+                entry,
+                &validation_layers,
+                &extensions,
+                cstr!("Vulkan Tutorial"),
+            )
+            .unwrap()
+        };
+
+        VulkanStateBuilder {
+            instance,
+            #[cfg(debug_assertions)]
+            debug_builder: |i| vku::DebugUtils::new(i).unwrap(),
+            surface_builder: |i| vku::Surface::new(i, window).unwrap(),
+            phy_devs_builder: |i| vku::PhysicalDev::list(i).unwrap(),
+            logic_dev_builder: |devs, s| {
+                let (idx, queues) = devs
+                    .iter()
+                    .copied()
+                    .filter(|&dev| is_physical_device_suitable(dev))
+                    .map(|dev| QueueFamiliesIndices::get(s, dev).unwrap())
+                    .enumerate()
+                    .flat_map(|(dev_idx, idxs)| Some(dev_idx).zip(idxs.zip()))
+                    .next()
+                    .expect("no suitable physical device found");
+                vku::LogicalDev::new(devs[idx], &queues).unwrap()
+            },
+        }
+        .build()
+    }
+}
+
 fn is_physical_device_suitable(dev: vku::PhysicalDev) -> bool {
     let prop = dev.properties();
     let feat = dev.features();
@@ -54,48 +129,5 @@ fn main() {
 
     let entry = unsafe { ash::Entry::load().unwrap() };
 
-    let validation_layers = vec![
-        cstr!(VK_LAYER_KHRONOS_validation).as_ptr(),
-        // ...
-    ];
-
-    let mut extensions = vec![
-        cstr!(VK_EXT_debug_utils).as_ptr(),
-        cstr!(VK_KHR_surface).as_ptr(),
-        // ...
-    ];
-
-    extensions.extend(
-        vku::Surface::extensions(&window)
-            .unwrap()
-            .into_iter()
-            .map(ffi::CStr::as_ptr),
-    );
-
-    let instance = unsafe {
-        vku::Instance::new(
-            &entry,
-            &validation_layers,
-            &extensions,
-            cstr!("Vulkan Tutorial"),
-        )
-        .unwrap()
-    };
-
-    let _debug_utils = vku::DebugUtils::new(&instance).unwrap();
-
-    let phy_devs = vku::PhysicalDev::list(&instance).unwrap();
-
-    let surface = vku::Surface::new(&instance, &window).unwrap();
-
-    let (idx, queues) = phy_devs
-        .iter()
-        .copied()
-        .filter(|&dev| is_physical_device_suitable(dev))
-        .map(|dev| QueueFamiliesIndices::get(&surface, dev).unwrap())
-        .enumerate()
-        .flat_map(|(dev_idx, idxs)| Some(dev_idx).zip(idxs.zip()))
-        .next()
-        .expect("no suitable physical device found");
-    let _logical_dev = vku::LogicalDev::new(phy_devs[idx], &queues).unwrap();
+    let vk_state = VulkanState::create(&entry, &window);
 }
