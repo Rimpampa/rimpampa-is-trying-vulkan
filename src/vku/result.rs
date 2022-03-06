ec261c83c0a7ea815d007940c7058b62353aba69
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2022-03-06 16:48:35 +0100

    Remove vku::Result type (now alias)

--- src/vku/result.rs
+++ src/vku/result.rs
@@ -1,11 +1,2 @@
-use ash::vk;
-
-/// Error related to a Vulkan operation
-#[derive(thiserror::Error, Debug)]
-pub enum Error {
-    /// An error directly returned by a Vulkan function
-    #[error("Vulkan error: {0:?}")]
-    Vulkan(#[from] vk::Result),
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Error = ash::vk::Result;
+pub type Result<T> = ash::prelude::VkResult<T>;
